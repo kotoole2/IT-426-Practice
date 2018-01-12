ddc0e872ba7684d9ddd6cfe7a3b0cad40b578013
kotoole2 <kotoole2@mail.greenriver.edu>
2018-01-11 17:24:13 -0800

    Added Package and a point class to represnt (x,y) coordinates.

---
+++ Test Project/src/making_changes/Point.java
@@ -0,0 +1,16 @@
+package making_changes;
+
+public class Point
+{
+    private double x;
+    private double y;
+
+    //bad naming conventions josh
+
+    public Point(double x, double y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+
+}
