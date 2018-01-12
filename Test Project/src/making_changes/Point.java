8eca57f16c813bff832b2d637e18251116dc62f5
kotoole2 <kotoole2@mail.greenriver.edu>
2018-01-11 17:39:02 -0800

    reverted and added a z axis

--- Test Project/src/making_changes/Point.java
+++ Test Project/src/making_changes/Point.java
@@ -6,13 +6,12 @@ public class Point
 {
     private double x;
     private double y;
+    private double z;
 
-    //bad naming conventions josh
-
-    public Point(double x, double y)
-    {
+    public Point(double x, double y, double z) {
         this.x = x;
         this.y = y;
+        this.z = z;
     }
 
     public double getX() {
@@ -27,23 +26,32 @@ public class Point
         return y;
     }
 
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public double getZ() {
+        return z;
+    }
+
+    public void setZ(double z) {
+        this.z = z;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Point point = (Point) o;
         return Double.compare(point.x, x) == 0 &&
-                Double.compare(point.y, y) == 0;
+                Double.compare(point.y, y) == 0 &&
+                Double.compare(point.z, z) == 0;
     }
 
     @Override
     public int hashCode() {
 
-        return Objects.hash(x, y);
-    }
-
-    public void setY(double y) {
-        this.y = y;
+        return Objects.hash(x, y, z);
     }
 
     @Override
@@ -51,6 +59,7 @@ public class Point
         return "Point{" +
                 "x=" + x +
                 ", y=" + y +
+                ", z=" + z +
                 '}';
     }
 }
